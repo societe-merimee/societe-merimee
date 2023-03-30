3136f7d71e8644f4e8817a61e662c67779e120f1
achtaitaipai <charles.cailleteau@gmail.com>
2023-03-30 09:48:13 +0200

    fractal theme skin

--- fractal.config.js
+++ fractal.config.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// require the Mandelbrot theme module
+const mandelbrot = require('@frctl/mandelbrot');
+
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create();
 
@@ -13,3 +16,12 @@ fractal.components.set('ext', '.njk');
 
 /* Tell Fractal where the documentation pages will live */
 fractal.docs.set('path', __dirname + '/style-guide/docs');
+
+// create a new instance with custom config options
+const theme = mandelbrot({
+    skin: 'black',
+    // any other theme configuration values here
+});
+
+// tell Fractal to use the configured theme by default
+fractal.web.theme(theme);
