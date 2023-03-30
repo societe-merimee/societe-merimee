046afb35b95d597cab5c8730d41c23fa0307ed5e
achtaitaipai <charles.cailleteau@gmail.com>
2023-03-30 12:17:26 +0200

    format

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,20 +1,20 @@
-const postcssImport = require("postcss-import");
-const postcssGlob = require("postcss-import-ext-glob");
-const postcssDesignTokens = require('@csstools/postcss-design-tokens');
-const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require('postcss-import')
+const postcssGlob = require('postcss-import-ext-glob')
+const postcssDesignTokens = require('@csstools/postcss-design-tokens')
+const postcssPresetEnv = require('postcss-preset-env')
 
 const config = {
-  plugins: [
-    postcssGlob,
-    postcssImport,
-    postcssDesignTokens,
-    postcssPresetEnv({
-      stage: 3,
-      features: {
-        "nesting-rules": true,
-      },
-    }),
-  ],
-};
+	plugins: [
+		postcssGlob,
+		postcssImport,
+		postcssDesignTokens,
+		postcssPresetEnv({
+			stage: 3,
+			features: {
+				'nesting-rules': true,
+			},
+		}),
+	],
+}
 
-module.exports = config;
+module.exports = config
