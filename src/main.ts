ae035710e4df19af51bb614b52deb356749c5198
achtaitaipai <charles.cailleteau@gmail.com>
2023-03-31 12:57:53 +0200

    component : details

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,7 @@ import '@fontsource/dm-sans'
 import '@fontsource/dm-serif-display'
 import '@fontsource/dm-serif-display/400-italic.css'
 import './style.css'
+
+import { defineDetailsCustomElement } from '../style-guide/components/02-moleculs/details/details'
+
+defineDetailsCustomElement()
