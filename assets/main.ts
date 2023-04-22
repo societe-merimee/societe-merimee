2d1b945721276de8c387166c154f95d2943eaefb
achtaitaipai <charles.cailleteau@gmail.com>
2023-04-22 10:19:41 +0200

    add details-utils dependencie

--- assets/main.ts
+++ assets/main.ts
@@ -3,12 +3,11 @@ import '@fontsource/dm-serif-display'
 import '@fontsource/dm-serif-display/400-italic.css'
 import './style.css'
 
-import { defineDetailsCustomElement } from '@components/02-moleculs/details/details'
 import { defineTabsCustomElement } from '@components/02-moleculs/tabs/tabs'
 import { defineSortableTableCustomElement } from '@components/03-organisms/table/sortableTable'
 import { defineSausageLinksCustomElement } from '@components/03-organisms/header/sausageLinks'
+import '@zachleat/details-utils'
 
-defineDetailsCustomElement()
 defineTabsCustomElement()
 defineSortableTableCustomElement()
 defineSausageLinksCustomElement()
